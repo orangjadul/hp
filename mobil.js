7657c30bb420504a4b46bd816083fa62c1dcb2a6
man <jegerdotkom@gmail.com>
2018-09-29 05:06:59 +0700

    Update mobil.js

--- mobil.js
+++ mobil.js
@@ -9,17 +9,6 @@ if (uri.indexOf("%3D%3D","%3D%3D") > 0) {
     window.history.replaceState({}, document.title, clean_uri);
 }
 var uri = window.location.toString();
-if (uri.indexOf("&m=0","&m=0") > 0) {
-  var clean_uri = uri.substring(0, uri.indexOf("&m=0"));
-    window.history.replaceState({}, document.title, clean_uri);
-}
-var uri = window.location.toString();
-if (uri.indexOf("?m=0","?m=0") > 0) {
-  var clean_uri = uri.substring(0, uri.indexOf("?m=0"));
-    window.history.replaceState({}, document.title, clean_uri);
-}
-
-var uri = window.location.toString();
 if (uri.indexOf("&m=1","&m=1") > 0) {
   var clean_uri = uri.substring(0, uri.indexOf("&m=1"));
     window.history.replaceState({}, document.title, clean_uri);
